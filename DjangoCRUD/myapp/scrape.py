77bf8be6e0747697dea8ba93bb0c37d2b64ec988
Isuranga Warnasooriya <59173608+Isuranga-2001@users.noreply.github.com>
2024-02-09 21:34:27 +0530

    Scrape files

--- DjangoCRUD/myapp/scrape.py
+++ DjangoCRUD/myapp/scrape.py
@@ -1,9 +1,52 @@
 import requests
 from bs4 import BeautifulSoup
+import mimetypes
 
-url = 'https://www.laptop.lk/index.php/product-category/laptops/'  # Replace with the target website URL
-reqs = requests.get(url)
-soup = BeautifulSoup(reqs.text, 'html.parser')
+url = 'https://www.laptop.lk/index.php/product-category/'  # Replace with the target website URL
+arr = []
 
-for link in soup.find_all('a'):
-    print(link.get('href'))
+f = open("urls.txt", "a")
+
+
+def get_body_content(base):
+    try:
+        response = requests.get(base)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        body_content = soup.body.text
+        return body_content
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+def is_html(base):
+    try:
+        response = requests.get(base)
+        content_type = response.headers['Content-Type']
+        return True
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
+
+
+def findAllUrls(base):
+    reqs = requests.get(base)
+    soup = BeautifulSoup(reqs.text, 'html.parser')
+
+    ln = len(base) - 1
+
+    for link in soup.find_all('a'):
+        sublink = link.get('href')
+        try:
+            if sublink[0:ln + 1] == base:
+                if is_html(base):
+                    f.append(sublink + '\n')
+                    if sublink not in arr:
+                        arr.append(sublink)
+                else:
+                    findAllUrls(sublink)
+        except:
+            continue
+
+f.close()
+findAllUrls(url)
